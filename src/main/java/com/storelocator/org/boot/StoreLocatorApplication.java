357cc06a28572d1ae9392f7ad1d2c7c35143aa72
arpit <itsarpitgupta@gmail.com>
2018-03-20 08:28:34 +0530

    project completed

--- src/main/java/com/storelocator/org/boot/StoreLocatorApplication.java
+++ src/main/java/com/storelocator/org/boot/StoreLocatorApplication.java
@@ -1,9 +1,12 @@
-package com.storelocator.org.store_locator;
+package com.storelocator.org.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class StoreLocatorApplication {
 
 	public static void main(String[] args) {
